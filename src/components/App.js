165b6e22cc6de32ec3b95a166f9745482755df79
pjavadi84 <pouya.javadi.resume@gmail.com>
2020-11-30 23:35:00 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,45 @@ class App extends React.Component {
     }
   }
 
+  // We need to pass a target as an object pointing to the onChangeType callback function JSX event. 
+  // We pass a target object because we want to target the element which may contain group of other nested 
+  // element inside to produce same behavior, to be selected. We want to say when this "target" being 
+  // selected we want to update the filter state. "..." implies that anything that is inside this "filter"
+  // state MUST stay out, and only the type should be selected and passed as a second argument, to be 
+  // THE VALUE that being seleced by the user inside the DOM. 
+  onChangeType = ({target: value}) => {
+    // 
+    
+    this.setState({
+      filters: {...this.state.filters, type: value}
+    })
+  }
+
+  fetchPets = () => {
+    this.endpoint = '/api/pets';
+
+    if (this.state.filters.type !== 'all') {
+      this.endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(this.endpoint)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets }));
+  }
+
+  onAdoptPet = petId=>{
+    const pets = this.state.pets.map(p => {
+      if(p.id === petId){
+        return {...p, isAdopted: true}
+      }else{
+        return p
+      }
+      
+    })
+
+    this.setState({pets: pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +63,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
